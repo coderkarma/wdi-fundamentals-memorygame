450c6e37e44eb9f88759d8c81d18bff6b7e998ae
karma <karmadrukpa11@gmail.com>
2019-01-15 00:37:00 -0800

    update with added functionlaity

---
+++ wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -0,0 +1,69 @@
+let cards = [{
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
+
+
+
+let cardMatch = false;
+let cardsInPlay = [];
+
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    cardMatch = true;
+    alert('You found a match!');
+  } else {
+    alert('Sorry, try again.');
+  }
+}
+
+function flipCard() {
+  if (!cardMatch) {
+    let cardId = this.getAttribute('data-id');
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
+    if (cardsInPlay.length === 2) {
+      checkForMatch();
+    }
+  }
+}
+
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+createBoard();
+
+// resetting the card 
+function reset() {
+  document.getElementById('game-board').innerHTML = '';
+   cardMatch = false;
+    cardsInPlay = [];
+    createBoard()
+}
+reset();
+
+document.querySelector('button').addEventListener('click', reset)
